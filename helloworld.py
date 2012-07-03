de2f1cea5940704c462245942dc3594443a2bfb8
Gavin Delaney <gavodel@gmail.com>
2012-07-03 16:01:59 +0100

    penalty game user input (direction) working

--- helloworld.py
+++ helloworld.py
@@ -8,10 +8,16 @@ def peno(player_name):
 	i = mix(1,3)
 	score = False
 	print "%s steps up" % player_name
-	raw_input("")
-	users_input = raw_input("if you want to shoot left type, l,  if you want to shoot right type, r,  if you want to shoot down the center type, c \n")
 	print "\n"
-	if(users_input == 'l' or 'r' or 'c'):
+	users_input = raw_input("If you want to shoot left type, l\nIf you want to shoot right type, r\nIf you want to shoot down the center type, c \ntype here: ")
+	print "\n"
+	while users_input not in ['l', 'r', 'c']:
+		print "INVALID CHOICE"
+		print "\n"
+		users_input = raw_input("If you want to shoot left type, l,\nIf you want to shoot right type, r,\nIf you want to shoot down the center type, c \ntype here: ")
+		print "\n"
+		
+	if users_input in ['l', 'r', 'c']:
 		if i == 1 or i==3:
 			print "And he scores"
 			score=True
@@ -20,26 +26,30 @@ def peno(player_name):
 		print "\n"
 		raw_input("")
 		return score
-	else:
-		print "start program again"
-
+	
+def print_team(team_name, team):
+	print team_name
+	
 team1 = ['Keith Baker','Gavin Delaney','Grant Baker','Luke Foley','Ben Foley']
 team2 = ['Kate Foley','Kelly Delaney','Rosie Delaney','Gwyneth Baker','Granny']
 raw_input("")	
 print "\n"	
-print "IRELAND"
 
+print_team("Ireland", team1)
 for i in range(0, 5):
 	print "%s "%team1[i]
 raw_input("")
-print "\nENGLAND"
 
+print_team("England", team2)
 for i in range(0, 5):
 	print "%s "%team2[i]
+
+
 print "\n"
 score1=0
 score2=0
 raw_input("")
+
 for i in range(0, 5):
 	scored1 = peno("[Ireland] %s "%team1[i])
 	print "\n"
@@ -48,8 +58,8 @@ for i in range(0, 5):
 		score1+=1
 	if(scored2 == True):
 		score2+=1
-	raw_input("")
 	print("%d-%d\n"%(score1,score2))
+	raw_input("")
 	print "\n"
 	
 if score1>score2:
