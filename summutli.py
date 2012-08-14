edb3b1e22faa28bf8307bbc7189bc1f6e2ffeb4b
Gavin Delaney <gavodel@gmail.com>
2012-08-14 15:39:17 +0100

    excercise 6.... http://www.ling.gu.se/~lager/python_exercises.html

---
+++ summutli.py
@@ -0,0 +1,13 @@
+def sum(list1):
+	y=0
+	for i in list1:
+		y+=i
+	return y
+def multiply(list2):
+	x=1
+	for i in list2: 
+		x=x*i
+	return x
+
+print sum([1, 2, 3, 4])
+print multiply([1, 2, 3, 4])
