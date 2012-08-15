7f7005acb446db7fdd4ca6d33a8ca5c87a22b445
Gavin Delaney <gavodel@gmail.com>
2012-08-15 15:58:01 +0100

    excercise 9.....http://www.ling.gu.se/~lager/python_exercises.html

---
+++ is_menmber.py
@@ -0,0 +1,16 @@
+def is_member(x,a):
+	for i in a:
+		if i == "%s"%x:
+			return True
+		else:
+			return False
+	
+
+x='g'
+a='gavin'
+y = is_member(x,a)
+
+if y:
+	print "%s is in the word %s"%(x,a)
+else:
+	print "%s is not in the word %s"%(x,a)
