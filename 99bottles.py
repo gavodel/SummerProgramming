c1db3bbd0cf14eae5f185e2c12786223a4f42ff7
Gavin Delaney <gavodel@gmail.com>
2012-08-21 13:44:58 +0100

    excercise 19......http://www.ling.gu.se/~lager/python_exercises.html

---
+++ 99bottles.py
@@ -0,0 +1,6 @@
+def bottles():
+	for i in range(99,0,-1):
+		print"%d bottles of beer on the wall, %d bottles of beer.Take one down, pass it around, %d bottles of beer on the wall." %(i,i,i-1)
+		
+bottles()
+	
