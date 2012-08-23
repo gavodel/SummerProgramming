3e5aa937997ae785ec734228f63232d0a305fc3a
Gavin Delaney <gavodel@gmail.com>
2012-08-23 22:54:38 +0100

    excercise 22......http://www.ling.gu.se/~lager/python_exercises.html

---
+++ ceaser.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+def ceaser(mix):
+	clean=''
+	for i in range(0,len(mix)):
+		u = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k', 'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A', 'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I', 'W':'J', 'X':'K', 'Y':'L', 'Z':'M'}
+		if mix[i] in u:
+			y = u[mix[i]]
+		else:
+			y = mix[i]
+		clean += y
+	return clean	
+
+print ceaser("Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!")
+
