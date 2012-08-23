0f910fa0b56df8f69add12d1ccfc0e64cd19eaca
Gavin Delaney <gavodel@gmail.com>
2012-08-23 17:13:54 +0100

    excercise 20......http://www.ling.gu.se/~lager/python_exercises.html
    working

--- engtoswe.py
+++ engtoswe.py
@@ -1,11 +1,12 @@
+#-*- coding: utf-8 -*-
 def translate(english ):
 	swede=[]
-	u= {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"år"}
-	for i in english:
+	u = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+	for i in range(0,len(english)):
 		y = u[english[i]]
 		swede.append(y)
 	return swede	
 
-h=translate("merry", "christmas", "and", "happy", "new", "year")
+h=translate(["merry", "christmas", "and", "happy", "new", "year"])
 print h
 
